8bd2168a702c011c0c1705431bbce7c634865494
Trung Nguyen <nguyenchitrung1310@gmail.com>
2020-12-19 16:28:48 +0700

    Handle app bar

--- e-learning/src/App.tsx
+++ e-learning/src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './App.scss';
+import AppMenu from './components/AppBar';
 import HomePage from './pages/Home';
+import { Box, Container, CssBaseline } from '@material-ui/core';
+import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <CssBaseline />
+
+      <AppMenu />
+      {/* <HomePage /> */}
+      <Container>
+        <Box my={2}>
+          {[...new Array(120)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+            )
+            .join('\n')}
+        </Box>
+      </Container>
     </div>
   );
 }
--- e-learning/src/pages/Home/index.tsx
+++ e-learning/src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
-import AppMenu from '../../components/AppBar';
 import './_homepage.scss';
 
 function HomePage() {
   return (
     <Grid className='home-page'>
-      <AppMenu />
       <Typography component='div' />
+      <Typography>ABC</Typography>
     </Grid>
   );
 }

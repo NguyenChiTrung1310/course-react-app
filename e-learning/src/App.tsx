fdbe6ae1e61ec50836cb7b10a30bd97bdba616d4
Trung Nguyen <nguyenchitrung1310@gmail.com>
2020-12-25 08:33:36 +0700

    Fix appbar and hover style checkbox

--- e-learning/src/App.tsx
+++ e-learning/src/App.tsx
@@ -2,34 +2,20 @@ import React from 'react';
 import AppMenu from './components/AppBar';
 import HomePage from './pages/Home';
 import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register'; 
-import {  Route, Switch} from 'react-router-dom';  
-import './App.scss'; 
+import RegisterPage from './pages/Register';
+import { Route, Switch } from 'react-router-dom';
+import './App.scss';
 import { LOGIN_PAGE, REGISTER_PAGE, HOME_PAGE } from './constants';
 
 function App() {
-  return (  
-      <div>
-         <AppMenu>  
-      </AppMenu> 
-      <Switch> 
-        <Route 
-          component={LoginPage}
-          exact
-          path={LOGIN_PAGE}
-         />
-          <Route 
-          component={RegisterPage}
-          exact
-          path={REGISTER_PAGE}
-         />
-         <Route 
-          component={HomePage}
-          exact
-          path={HOME_PAGE}
-         />
-      </Switch> 
-      </div>
+  return (
+    <AppMenu>
+      <Switch>
+        <Route component={LoginPage} exact path={LOGIN_PAGE} />
+        <Route component={RegisterPage} exact path={REGISTER_PAGE} />
+        <Route component={HomePage} exact path={HOME_PAGE} />
+      </Switch>
+    </AppMenu>
   );
 }
 

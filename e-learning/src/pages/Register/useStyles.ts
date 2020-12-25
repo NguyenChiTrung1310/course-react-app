668a9823196f3250888fb32ae82f64f1e6aa2d7b
Trung Nguyen <nguyenchitrung1310@gmail.com>
2020-12-25 08:38:55 +0700

    Fix over-flow css

--- e-learning/src/pages/Register/useStyles.ts
+++ e-learning/src/pages/Register/useStyles.ts
@@ -16,12 +16,11 @@ const useStyles: () => Record<
  = makeStyles((theme: Theme) => ({
   root: {
     width: '100%',
-    height: '90vh',
     display: 'flex',
     flexDirection: 'row',
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(0, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',

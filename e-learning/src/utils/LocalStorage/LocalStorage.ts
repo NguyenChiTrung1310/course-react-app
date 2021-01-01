3801063336cb5f8de1503c984bd5c6108046f7e9
Hai Vu <duchaivu1997@gmail.com>
2021-01-01 23:06:43 +0700

    [EL-2] add get/set localstorage

--- e-learning/src/utils/LocalStorage/LocalStorage.ts
+++ e-learning/src/utils/LocalStorage/LocalStorage.ts
@@ -1,7 +1,24 @@
-import {LOCAL_STORAGE_TOKEN_KEY} from '../../constants';
+import {LOCAL_STORAGE_TOKEN_KEY, LOCAL_STORAGE_CREDENTIALS_KEY} from '../../constants';
+
+//set credentials
+export const setDataFromLocalStorage=(data: any)=>{
+    return localStorage.setItem(LOCAL_STORAGE_CREDENTIALS_KEY, data);
+}
+
 
 //get token
 export const getDataFromLocalStorage= () =>{
     const token = localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
     return token;
+}
+
+//get credentials
+export const getDataFromCredentials=(data: any)=>{
+    return localStorage.getItem(data);
+}
+
+
+//clear localstorage
+export const clearStoreFromlocalStorage=()=>{
+    return localStorage.clear();
 }

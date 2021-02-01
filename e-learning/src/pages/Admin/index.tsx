7d2faa4971a61920fc7f5c1191385513f23cabd4
Hai Vu <duchaivu1997@gmail.com>
2021-02-01 08:36:11 +0700

    [EL-7] update status get list user

--- e-learning/src/pages/Admin/index.tsx
+++ e-learning/src/pages/Admin/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { adminAction } from '../../features/admin/adminAction';
 import {
   makeStyles,
   useTheme,
@@ -13,18 +14,20 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
-import { adminAction } from '../../features/admin/adminAction';
-
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import { Grid, Typography } from '@material-ui/core';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
-
-function createData(name: string, calories: number, fat: number) {
-  return { name, calories, fat };
-}
 
 function createData1(
   account: string,
@@ -35,13 +38,111 @@ function createData1(
   return { account, name, typeUser, email };
 }
 
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onChangePage: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
+}
+
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  })
+);
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label='first page'>
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label='previous page'>
+        {theme.direction === 'rtl' ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label='next page'>
+        {theme.direction === 'rtl' ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label='last page'>
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
 export default function Admin() {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     dispatch(adminAction());
   }, []);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const dataListUser = useSelector(
     (state: any) => state.admin.listUserResponse.response
@@ -57,32 +158,63 @@ export default function Admin() {
     return createData1(taiKhoan, hoTen, maLoaiNguoiDung, email);
   });
 
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rowRender.length - page * rowsPerPage);
+
   return (
-    <div style={{ width: '800px' }}>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label='simple table'>
-          <TableHead>
-            <TableRow>
-              <TableCell>Account</TableCell>
-              <TableCell align='right'>Name</TableCell>
-              <TableCell align='right'>Type User </TableCell>
-              <TableCell align='right'>Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rowRender.map((rowRender: any) => (
-              <TableRow key={rowRender.account}>
-                <TableCell component='th' scope='row'>
-                  {rowRender.account}
-                </TableCell>
-                <TableCell align='right'>{rowRender.name}</TableCell>
-                <TableCell align='right'>{rowRender.typeUser}</TableCell>
-                <TableCell align='right'>{rowRender.email}</TableCell>
+    // style={{ width: '800px' }}
+    <div>
+      <Paper>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Account</TableCell>
+                <TableCell align='right'>Name</TableCell>
+                <TableCell align='right'>Type User </TableCell>
+                <TableCell align='right'>Email</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? rowRender.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : rowRender
+              ).map((row: any) => (
+                <TableRow key={row.account}>
+                  <TableCell component='th' scope='row'>
+                    {row.account}
+                  </TableCell>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.tyleUser}</TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            // colSpan={3}
+            component='div'
+            count={rowRender.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            // SelectProps={{
+            //   inputProps: { 'aria-label': 'rows per page' },
+            //   native: true,
+            // }}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+            ActionsComponent={TablePaginationActions}
+          />
+        </TableContainer>
+      </Paper>
     </div>
   );
 }
